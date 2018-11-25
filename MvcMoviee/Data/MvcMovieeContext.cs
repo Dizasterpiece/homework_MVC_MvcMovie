5c9889c64412c2aa9e2b6687f72eb98e73001413
Bogdan <bdikov64@gmail.com>
2018-11-25 23:42:19 +0200

    hw_MvcMovie

---
+++ MvcMoviee/Data/MvcMovieeContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMoviee.Models
+{
+    public class MvcMovieeContext : DbContext
+    {
+        public MvcMovieeContext (DbContextOptions<MvcMovieeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMoviee.Models.Movie> Movie { get; set; }
+    }
+}
